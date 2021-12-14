5f769e7a6d9fb81cf50559cc904a04d3821250f6
Gustavo Araújo <gustavo.avk@msn.com>
2021-12-14 08:06:40 -0300

    Adicionando fotos da API

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,69 @@
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+ 
+
+  <ul class="lista-fotos">
+    <li class="lista-fotos-item" v-for="foto of fotos">
+
+      <div class="painel">
+        <h2 class="painel-titulo">
+          {{ foto.titulo }}
+        </h2>
+        <div class="painel-conteudo">
+          <img :src="foto.url" :alt="foto.titulo"/>
+        </div>
+      </div>
+
+    </li>
+  </ul>
+
   </div>
+
+
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+
+  data(){
+
     return {
-      msg: 'Welcome to Your Vue.js App'
+      titulo: 'Do.ogs',
+      fotos: []
     }
+  },
+
+  created() {
+
+
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+
   }
+
+ 
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+.corpo{
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+  }
 
-h1, h2 {
-  font-weight: normal;
-}
+  .centralizado{
+    text-align: center;  
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .lista-fotos{
+    list-style: none;
+  }
+  .lista-fotos-item{
+    display: inline-block
+    ;
+  }
 </style>
